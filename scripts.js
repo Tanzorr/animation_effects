a28aa95d5bf054b8cbf5b0c0dba86f349a84ced1
tanzorr <alexx1984@ukr.net>
2020-10-31 16:33:20 +0200

    modal window

--- scripts.js
+++ scripts.js
@@ -1,9 +1,41 @@
+/*
+animated string
+ */
+
 function animate(id) {
     var node = document.getElementById(id).childNodes[0];
     var text = node.data;
     setInterval(function () {
         text = text.substring(1) + text[0];
         node.data = text;
-    }, 125); //интервал прокрутки, мс
+    }, 150); //интервал прокрутки, мс
 }
 window.addEventListener('load', function (e) { animate('text_line'); }, false);
+
+/*
+end animated string
+ */
+
+/*
+modal window
+ */
+var modal = document.getElementById("my_modal");
+var btn = document.getElementById("btn_modal_window");
+var span = document.getElementsByClassName("close_modal_window")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+/*
+modal window
+ */
